d4354a98363493fa12337a583e0ca95e8ae4240b
Jemina Sy <jemina.c.sy@gmail.com>
2023-08-14 17:21:14 +0800

    test

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ const currentURL = window.location.href;
 // Function to load content from a file and set it as innerHTML
 async function loadContentFromFile(filePath) {
   try {
-    const response = await fetch(`../page/${filePath}`);
+    const response = await fetch(filePath);
+    // const response = await fetch(`../page/${filePath}`);
+
     const content = await response.text();
     contentContainer.innerHTML = content;
   } catch (error) {
@@ -21,7 +23,7 @@ function loadContent(url) {
   } else if (url.includes("contact")) {
     loadContentFromFile("contact.html");
   } else {
-    loadContentFromFile("home.html");
+    loadContentFromFile("../page/home.html");
   }
 }
 
