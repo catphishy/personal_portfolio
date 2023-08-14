19f012c3de4e262bb975e66ec48dd36df0546e90
Jemina Sy <jemina.c.sy@gmail.com>
2023-08-14 22:10:17 +0800

    test

---
+++ js/layout.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'beige': '#F5ECE5',
+                'purple': '#825589',
+                'bright-purple': '#9e74a4',
+                'dark-purple': '#624467',
+            },
+            fontFamily: {
+              lekton: ['Lekton', 'sans-serif'],
+            }
+        }
+    }
+}
