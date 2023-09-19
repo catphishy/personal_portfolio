978eee5990c23e62240d33f040bc55cc75ba9b8c
Jemina Sy <jemina.c.sy@gmail.com>
2023-09-19 17:02:28 +0800

    switched to dark mode

--- js/layout.js
+++ js/layout.js
@@ -6,6 +6,9 @@ tailwind.config = {
                 'purple': '#825589',
                 'bright-purple': '#9e74a4',
                 'dark-purple': '#624467',
+                'dark-blue': '#1A355B',
+                // 'bright-purple': '#BF40BF',
+                'black': '#091A32',
             },
             fontFamily: {
               lekton: ['Lekton', 'sans-serif'],
